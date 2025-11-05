fda25ec4244ceccacefa6e18b63dd438fde2456c
Gautam <gautamrawat52002gmail.com>
2025-11-05 23:55:50 +0530

    routes updatessssss

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
 import React, { useContext } from 'react';
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import AuthContext from './context/AuthContext';
 
 import Layout from './components/Layout';
